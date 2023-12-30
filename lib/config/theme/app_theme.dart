e215a3e294145af0fbcf772f0f335287536825c0
Diego Pagini <dhpagini@knowmadmood.com>
2023-12-30 11:27:52 +0100

    theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'selectedColor must be greather than 0.'),
+        assert(selectedColor < colorList.length - 1,
+            'selectedColor must be less or equiat than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        colorSchemeSeed: colorList[selectedColor],
+        useMaterial3: true,
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 0).getTheme(),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
