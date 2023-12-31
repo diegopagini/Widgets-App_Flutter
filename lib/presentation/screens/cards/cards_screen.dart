27b60abc791b3685628501ee8166c54f981e5961
Diego Pagini <dhpagini@knowmadmood.com>
2023-12-31 14:19:45 +0100

    cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -7,16 +7,6 @@ const cards = <Map<String, dynamic>>[
   {'elevation': 3.0, 'label': 'Elevation 3'},
   {'elevation': 4.0, 'label': 'Elevation 4'},
   {'elevation': 5.0, 'label': 'Elevation 5'},
-  {'elevation': 6.0, 'label': 'Elevation 6'},
-  {'elevation': 7.0, 'label': 'Elevation 7'},
-  {'elevation': 0.0, 'label': 'Elevation 0'},
-  {'elevation': 1.0, 'label': 'Elevation 1'},
-  {'elevation': 2.0, 'label': 'Elevation 2'},
-  {'elevation': 3.0, 'label': 'Elevation 3'},
-  {'elevation': 4.0, 'label': 'Elevation 4'},
-  {'elevation': 5.0, 'label': 'Elevation 5'},
-  {'elevation': 6.0, 'label': 'Elevation 6'},
-  {'elevation': 7.0, 'label': 'Elevation 7'},
 ];
 
 class CardsScreen extends StatelessWidget {
@@ -46,7 +36,13 @@ class _CardsView extends StatelessWidget {
         children: [
           // For cycle
           ...cards.map((card) =>
-              _CardType1(label: card['label'], elevation: card['elevation']))
+              _CardType1(label: card['label'], elevation: card['elevation'])),
+
+          ...cards.map((card) =>
+              _CardType2(label: card['label'], elevation: card['elevation'])),
+
+          ...cards.map((card) =>
+              _CardType3(label: card['label'], elevation: card['elevation']))
         ],
       ),
     );
@@ -76,6 +72,82 @@ class _CardType1 extends StatelessWidget {
             Align(
               alignment: Alignment.bottomLeft,
               child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(
+            color: colors.outline,
+          )),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: const Icon(Icons.more_vert_outlined),
+                  onPressed: () {},
+                )),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(
+            color: colors.outline,
+          )),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: const Icon(Icons.more_vert_outlined),
+                  onPressed: () {},
+                )),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
             )
           ],
         ),
