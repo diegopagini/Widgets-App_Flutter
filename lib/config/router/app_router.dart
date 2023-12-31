7b0b7f0bf8adb7b7a16a095e9fbf6c3bd30b9252
Diego Pagini <dhpagini@knowmadmood.com>
2023-12-31 09:40:02 +0100

    go router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,14 +3,17 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
+    name: HomeScreen.name,
     path: '/',
     builder: (context, state) => const HomeScreen(),
   ),
   GoRoute(
+    name: ButtonsScreen.name,
     path: '/buttons',
     builder: (context, state) => const ButtonsScreen(),
   ),
   GoRoute(
+    name: CardsScreen.name,
     path: '/cards',
     builder: (context, state) => const CardsScreen(),
   )
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
